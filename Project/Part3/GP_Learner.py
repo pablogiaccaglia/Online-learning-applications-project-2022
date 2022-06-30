842956dd4d19b435ff9589c6a57711d8d05d5d82
ricky <riccardo1.ambrosini@mail.polimi.it>
2022-06-30 17:46:54 +0200

    GP_Learner (UCB like) is almost done

--- Project/Part3/GP_Learner.py
+++ Project/Part3/GP_Learner.py
@@ -17,32 +17,34 @@ WE ASSUME THAT THE BID IS OPTIMAL THROUGHOUT THE DAY, GIVEN THE BUDGET
 '''
 # todo: how do I init the knapsack table?
 # todo: what's the feedback? I think it's the aggregated gross profit for each product (campaign).
-#       In this case I would divide by the sum of users' expected profit to estimate the alphas.
-# todo: what are arms in this case? If it's a discrete number of bids, GP_Campaign should be modified accordingly.
 
 
 class GP_Learner(Learner):
-    def __init__(self, arms,n_campaigns): # arms are the budgets (e.g 0,10,20...)
-        self.n_arms = len(arms)
-        super().__init__(self.n_arms)  # not sure we should keep this
+    def __init__(self, arms, n_campaigns):  # arms are the budgets (e.g 0,10,20...)
+        self.n_arms = len(arms)  # there should be another solution to pass from numerical budget to index
+        super().__init__(self.n_arms, n_campaigns)
         self.means = np.ones((self.n_arms, n_campaigns)) * 3e1  # table of rewards for knapsack. Probably not zero??
         self.sigmas = np.ones((self.n_arms, n_campaigns)) * 2e1
 
-    # todo: fix this. The arm will be pulled according to the knapsack solution, pulling one arm (choosing one
-    #       budget) for each campaign. We will have to look at the last iteration of allocation
+    # The arm will be pulled according to the knapsack solution, pulling one arm (choosing one
+    # budget) for each campaign. The last iteration of allocation from knapsack contains the best solution
     def pull_arm(self) -> np.array:
-        k = Knapsack(rewards=np.random.normal(self.means,self.sigmas).clip(0.0), budgets=self.arms)
+        k = Knapsack(rewards=np.random.normal(self.means, self.sigmas).clip(0.0), budgets=self.arms)
         k.solve()
-        idx = k.allocations[-1]
-        #idx = np.argmax(np.random.normal(self.means, self.sigmas), axis=1)
-        return idx
+        super_arm = k.allocations[-1][-1]
+        return super_arm
 
+    # super_arm like: [20,0,10,30,40] reward like: [80.0,44.3,101.4,200.3,20.3]
+    def update_observations(self, super_arm, rewards):
+        for campaign, budget in enumerate(super_arm):
+            self.rewards_per_arm[np.where(self.arms == budget)][campaign].append(rewards[campaign])
+        super().update_observations(rewards)
 
-    def update(self, pulled_arm, reward):
-        self.t += 1
-        self.update_observations(pulled_arm, reward)
-        self.means[pulled_arm] = np.mean(self.rewards_per_arm[pulled_arm])
-        n_samples = len(self.rewards_per_arm[pulled_arm])
-
-        if n_samples > 1:
-            self.sigmas[pulled_arm] = np.std(self.rewards_per_arm[pulled_arm]) / n_samples
+    def update(self, super_arm, rewards):
+        self.update_observations(super_arm, rewards)
+        for campaign, budget in enumerate(super_arm):
+            arm = np.where(self.arms == budget)
+            self.means[arm][campaign] = np.mean(self.rewards_per_arm[arm][campaign])
+            n_samples = len(self.rewards_per_arm[arm][campaign])
+            if n_samples > 1:
+                self.sigmas[arm][campaign] = np.std(self.rewards_per_arm[arm][campaign])/n_samples
--- Project/Part3/Learner.py
+++ Project/Part3/Learner.py
@@ -1,16 +1,14 @@
 import numpy as np
 
-'''
-copypasta
-'''
-class Learner():
 
-    def __init__(self, n_arms):
+class Learner:
+
+    def __init__(self, n_arms, n_campaigns):
         self.n_arms = n_arms
+        self.n_campaigns = n_campaigns
         self.t = 0
-        self.rewards_per_arm = [[] for _ in range(n_arms)]
+        self.rewards_per_arm = [[[] for _ in range(n_campaigns)] for _ in range(n_arms)]  # shape: (n_arms,n_campaigns)
         self.collected_rewards = np.array([])
 
-    def update_observations(self, pulled_arm, reward):
-        self.rewards_per_arm[pulled_arm].append(reward)
+    def update_observations(self, reward):
         self.collected_rewards = np.append(self.collected_rewards, reward)
